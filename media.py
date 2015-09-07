99c66374462757cd1fe2eef622e51a3f81fe6207
Justin Hester <justinvhester@users.noreply.github.com>
2015-09-06 19:42:11 -0500

    Clean up of code format per pep8

--- media.py
+++ media.py
@@ -1,5 +1,4 @@
-import webbrowser
-# define a Movie class which contains all the characteristics 
+# define a Movie class which contains all the characteristics
 class Movie():
     """ This class provides a way to store movie related information.
 
@@ -18,15 +17,18 @@ class Movie():
         "PG-13": "https://upload.wikimedia.org/wikipedia/commons/c/c0/RATED_PG-13.svg",
         "R": "https://upload.wikimedia.org/wikipedia/commons/7/7e/RATED_R.svg"
     }
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, film_rating):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube, film_rating):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-        # Validate film_rating value against keys in the MPAA_RATING_IMGS class dict
+        # Validate film_rating value against keys in the
+        # MPAA_RATING_IMGS class dict
         if film_rating in self.MPAA_RATING_IMGS.keys():
             # if the value is valid, set self.film_rating to the URL value
             self.film_rating = self.MPAA_RATING_IMGS[film_rating]
         else:
             # if not, use a not available image
             self.film_rating = "https://upload.wikimedia.org/wikipedia/commons/e/e7/Not-available.svg"
+
